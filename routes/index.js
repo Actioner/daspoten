85f28e80623b12262b3526733679aeeda899f2d0
unknown <jfrodriguez@L-ITE-68VKTW1.idb.iadb.org>
2015-08-26 22:16:13 -0400

    Spot down vote by user. Spot invalidation. User administrator. Device type.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'daspoten' });
+  res.render('index', { title: 'haibu' });
 });
 
 module.exports = router;
